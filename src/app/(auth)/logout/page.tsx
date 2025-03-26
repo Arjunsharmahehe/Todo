932b6562daf52fb38a7dc53bb6ccacd29700c2ff
Arjunsharmahehe <whocaresaboutarjun@gmail.com>
2025-03-27 00:49:28 +0530

    fixed build errors

--- src/app/(auth)/logout/page.tsx
+++ src/app/(auth)/logout/page.tsx
@@ -19,7 +19,7 @@ export default function Logout() {
     return(
         <div className="flex flex-col justify-center items-center mt-7">
         <div className="flex items-center flex-col p-4 border-2 border-neutral-800 rounded-md max-w-96 gap-3 w-5/6 sm:4/5 md:w-3/4 lg:w-1/2">
-            <h1 className="text-2xl font-bold">You aren't logged in!</h1>
+            <h1 className="text-2xl font-bold">You aren&apos;t logged in!</h1>
             <Link href="/login"
                     className="border border-green-400 text-green-400 text-md font-semibold rounded-md px-4 py-1 hover:bg-green-950 hover:text-green-300    ">
                 Login
